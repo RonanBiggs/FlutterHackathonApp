f79c88cf9e1ae0ff0389bacc34db788f4f36cbb4
yennyyma <mayennyy@gmail.com>
2025-02-15 17:21:20 -0800

    kjkjkk

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'post_widget.dart';
 import 'example_page.dart';
-import 'add_posts.dart';
+import '../ios/add_posts.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -50,6 +50,20 @@ class _MyHomePageState extends State<MyHomePage> {
   }
   */
 
+  bool isLoggedIn = false;
+
+  void _loginSuccess() {
+    setState(() {
+      isLoggedIn = true;
+    });
+  }
+
+  void _logout() {
+    setState(() {
+      isLoggedIn = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
    return Scaffold(
@@ -71,7 +85,24 @@ actions: <Widget>[
               Navigator.pushNamed(context, '/create');
             },
           ),
-
+          IconButton(
+            icon: Icon(isLoggedIn ? Icons.verified_user_rounded : Icons.person),
+            onPressed: () {
+              if (isLoggedIn) {
+                _logout();
+              }
+              else {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => LoginPage(
+                      onLoginSuccess: _loginSuccess, // Pass callback for login success
+                    ),
+                  ),
+                );
+              }
+            },
+          ),
         ],
         
       ),
@@ -147,6 +178,76 @@ actions: <Widget>[
         tooltip: 'Increment',
         child: const Icon(Icons.add),*/
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  final Function onLoginSuccess;
+  const LoginPage({super.key, required this.onLoginSuccess});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final String validEmail = 'user@examplemail.com';
+  final String validPassword = 'abc123';
+  String errorMessage = '';
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  Future<void> _login() async {
+    if (_emailController.text == validEmail &&
+        _passwordController.text == validPassword) {
+      widget.onLoginSuccess();
+      Navigator.of(context).pop();
+    } else {
+      setState(() {
+        errorMessage = 'Invalid email address or password';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Sign in')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email:',
+                ),
+              ),
+              const SizedBox(height: 16),
+              TextField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password:',
+                ),
+              ),
+              const SizedBox(height: 24),
+              ElevatedButton(onPressed: _login, child: const Text('Submit')),
+              if (errorMessage.isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0),
+                  child: Text(
+                    errorMessage,
+                    style: const TextStyle(color: Colors.red),
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
